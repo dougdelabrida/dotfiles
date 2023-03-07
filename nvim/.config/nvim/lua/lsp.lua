8399dd130fd145c78f43716c1471ec6ff91925a7
Douglas Delabrida <dougdelabrida@gmail.com>
2023-03-07 10:54:52 +0000

    Fix ts server name

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -3,7 +3,7 @@ require('mason').setup({
 })
 
 require('mason-lspconfig').setup({
-  ensure_installed = { 'rust_analyzer', 'tsserver', 'elixirls' },
+  ensure_installed = { 'rust_analyzer', 'typescript-language-server', 'elixirls' },
 })
 
 vim.diagnostic.config({
